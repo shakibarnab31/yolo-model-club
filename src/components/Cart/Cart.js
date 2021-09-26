ba685982ebdcee6603acd8fb841abb49f24ad953
shakibarnab31 <arnab.muslim@gmail.com>
2021-09-26 23:20:59 +0600

    cart and design fixed

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,19 +3,19 @@ import './Cart.css'
 
 const Cart = (props) => {
     const { cart } = props;
-
-
-    let addedModel = '';
     let total = 0;
     for (const model of cart) {
         total = total + model.salary;
-        addedModel = addedModel + ' ' + model.name;
     }
     return (
         <div className="cart">
-            <h3>Model Added:{cart.length}</h3>
-            <h3>Total:${total}</h3>
-            <h5>Added Model: {addedModel}</h5>
+            <h3>Model Added: {cart.length}</h3>
+            <h3>Total: ${total}</h3>
+            <ul>
+                {
+                    cart.map(model => <li>{model.name}</li>)
+                }
+            </ul>
         </div>
     );
 };
